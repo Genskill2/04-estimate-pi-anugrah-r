c83857cf02da35b1493967b525a1c26e8ef3ec56
anugrah-r <84712467+anugrah-r@users.noreply.github.com>
2021-06-01 13:32:31 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -17,19 +17,24 @@ float wallis_pi(int)
   return pi*2;
 }
 
-int main(void) {
+int main(void) 
+{
   float pi;
-  for (int i=0; i<5; i++) {
+  for (int i=0; i<5; i++) 
+  {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) > 0.15)) {
+    if (!(fabs(pi - M_PI) > 0.15)) 
+    {
       printf("Estimate with just %d iterations is %f which is too accurate.\n", i, pi);
       abort();
     }
   }
 
-  for (int i=500; i<3000; i++) {
+  for (int i=500; i<3000; i++) 
+  {
     pi = wallis_pi(i);
-    if (!(fabs(pi - M_PI) < 0.01)) {
+    if (!(fabs(pi - M_PI) < 0.01)) 
+    {
       printf("Estimate with even %d iterations is %f which is not accurate enough.\n", i, pi);
       abort();
     }
